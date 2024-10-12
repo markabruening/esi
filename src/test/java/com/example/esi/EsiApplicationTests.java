4ab30dcdfc22ef049aa8f65ded96c4f6d9c4fbb4
Mark Bruening <markabruening@gmail.com>
2024-10-12 17:18:00 -0400

    Initial Commit

---
+++ src/test/java/com/example/esi/EsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
